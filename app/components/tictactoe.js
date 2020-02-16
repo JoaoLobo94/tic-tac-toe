6530f1ff601d39e711750decd7382c20fdbe219b
joao lobo <joao.lobom@gmail.com>
2020-02-16 15:06:53 +0100

    working winner

--- app/components/tictactoe.js
+++ app/components/tictactoe.js
@@ -2,14 +2,26 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { set } from "@ember/object";
-import { getOwner } from '@ember/application';
+import { getOwner } from "@ember/application";
 
 export default class TictactoeComponent extends Component {
-  
   get tracker() {
-    return getOwner(this).lookup('service:tracker');
+    return getOwner(this).lookup("service:tracker");
   }
   @tracked xTurn = false;
+  @tracked xWins = false
+  @tracked oWins = false
+
+  winningCombo = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+  ];
 
   @tracked grid = {
     1: null,
@@ -23,10 +35,9 @@ export default class TictactoeComponent extends Component {
     9: null
   };
 
-
   @action
-   increment() {
-    if ( this.tracker.count > 0) {
+  increment() {
+    if (this.tracker.count > 0) {
       this.tracker.count--;
       if (this.tracker.count % 2 == 0) {
         this.xTurn = true;
@@ -40,21 +51,45 @@ export default class TictactoeComponent extends Component {
   played(gridParam) {
     if (this.grid[gridParam] == null) {
       if (this.tracker.count % 2 == 0) {
-        set(this.grid, gridParam, "x");
+        set(this.grid, gridParam, 'x');
       } else {
-        set(this.grid, gridParam, "circle");
+        set(this.grid, gridParam, 'circle');
       }
     }
   }
   @action
-  winner(gridParam) {
-    this.tracker.winningCombo.some(combination =>{
-        combination.every(index =>{
-        gridParam[index].classList.contains('x')
-      })
-    })
+  winnerArr(Piece) {
+    return Object.keys(this.grid).filter(key => {
+     return this.grid[key] === Piece
+    }).map(Number)
+  }
+
+
+  @action
+  isWinner(gamePiece){
+    for (let i = 0; i < this.winningCombo.length; i++) {
+     let victory = this.winningCombo[i].every(r=>this.winnerArr(gamePiece).indexOf(r) >= 0)      
+      if (victory == true) {
+        return true
+      }
+      else if (Object.values(this.grid).includes(null)==false) {
+        return "Draw"
+      }
+      else{
+        return false
+      }
+    }
+  }
+
+  @action
+  winner() {
+    if (this.isWinner('x')) {
+      this.xWins == true
+    } else if (this.isWinner('circle')){
+      this.oWins == true
+    }
+    else {
+      return "draw"
+    }
   }
 }
-
-
-
--- app/services/tracker.js
+++ app/services/tracker.js
@@ -1,17 +1,8 @@
-import Service from '@ember/service';
+import Service from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 
 export default class TrackerService extends Service {
-    @tracked count = 9;
-    winningCombo = [
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 5, 9],
-        [3, 5, 7]
-      ];
-    
+  @tracked count = 9;
+  @tracked xVictory = 0
+  @tracked oVictory = 0
 }
